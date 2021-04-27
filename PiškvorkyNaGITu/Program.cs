b7df79c8eeefe983cd9997b1eeb4268748d3f62a
Adam Skalka <skalka@spsejecna.cz>
2021-04-27 10:09:48 +0200

    Test

--- PiškvorkyNaGITu/Program.cs
+++ PiškvorkyNaGITu/Program.cs
@@ -18,7 +18,7 @@ namespace PiškvorkyNaGITu
             Application.SetCompatibleTextRenderingDefault(false);
             /*tets*/
             Application.Run(new Form1());
-            /*taky*/
+            /*taky2*/
         }
     }
 }
