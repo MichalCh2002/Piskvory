ec00235a55d42426f66dc2c006de0eaeb36c9dd4
Adam Skalka <skalka@spsejecna.cz>
2021-04-27 10:45:54 +0200

    Prvni promenne

--- PiškvorkyNaGITu/Program.cs
+++ PiškvorkyNaGITu/Program.cs
@@ -18,9 +18,15 @@ namespace PiškvorkyNaGITu
             Application.SetCompatibleTextRenderingDefault(false);
             /*tets*/
             Application.Run(new Form1());
+
         }
 
-        static int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        //pole pro mrizku piskvorek
+        static int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+        static int player = 1;  //hrac
+        static int choice;  //pozice vyberu hrace
+
+        static int flag = 0;    //promenna pro identifikaci viteze
 
 
 
